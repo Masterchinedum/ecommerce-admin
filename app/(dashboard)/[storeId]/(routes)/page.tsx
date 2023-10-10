59a9856416bc4acf41ae102c291bc57961688338
Masterchinedum <chinedu02k@gmail.com>
2023-10-10 02:24:22 +0100

    more work add more components and stuffs

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
-const Dashboardpage = () => {
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+    params: {storeId: string}
+};
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    });
     return (
-        <div>
-            This is a Dashboard!
+        <div> 
+            Active Store: {store?.name}
         </div>
-    );
+    )
 }
 
-export default Dashboardpage;
+export default DashboardPage;

0f6df657fbb5f1fff358e57722e1906b2a32d378
Masterchinedum <chinedu02k@gmail.com>
2023-11-15 18:36:39 +0100

    Update settings page with new form component

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,7 @@
+export const SettingsForm = () => {
+    return (
+        <div> 
+            settings form
+        </div>
+    );
+}
--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,8 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { SettingsForm } from "./components/settings-form";
+
 interface SettingsPageProps {
     params: {
         storeId: string;

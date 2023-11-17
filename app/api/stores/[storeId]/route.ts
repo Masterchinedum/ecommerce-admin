35713e03c59d8f49dc6b5826547d5e04e040c949
Masterchinedum <chinedu02k@gmail.com>
2023-11-17 03:47:01 +0100

    Update build manifest and app paths manifest

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,10 +1,17 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
 export async function PATCH (
     req: Request,
     { params }: { params: { storeId: string } }
 ) {
     try {
+        const { !userId } = auth();
+
+        
 
     } catch (error) {
-        
+        console.log('[STORE_PATCH]' error);
+        return new NextResponse("internal error", {status:500});
     }
 }
